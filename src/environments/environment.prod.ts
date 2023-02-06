68b318b231a4984bc8c0212e237eee1a3b93baf8
janhavi.badgujar <janhavi.badgujar@thinkitive.com>
2023-02-06 14:30:42 +0530

    Changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   environmentName: 'PROD',
   baseUrl: 'https://mindoptimizerbackend.thinkathon.co.in',
-  pusherKey:'4fc51f255f4be21a067d'
+  pusherKey:''
+  //pusherKey:'4fc51f255f4be21a067d'
 };
