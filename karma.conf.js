2d62cea218f477875ed82256db6b40d60e6f0745
janhavi.badgujar <janhavi.badgujar@thinkitive.com>
2023-02-07 13:23:33 +0530

    Key Changes

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/intrinsicpower-frontend'),
+      dir: require('path').join(__dirname, './coverage/mind-optimizers-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
